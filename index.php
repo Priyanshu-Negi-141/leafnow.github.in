acab68d0954ce3b658ec7a713fcc702412a0c93e
Priyanshu-Negi-141 <priyanshunegi121@gmail.com>
2021-10-03 19:27:13 +0530

    leaf now

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");  //include connection file
+error_reporting(0);  // using to hide undefine undex errors
+session_start(); //start temp session until logout/browser closed
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>LEAF NOW</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet"> </head>
+
+<body class="home">
+    
+        <!--header starts-->
+        <header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php">LEAF NOW</a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="store.php">Store<span class="sr-only"></span></a> </li>
+                            
+                           
+							<?php
+						if(empty($_SESSION["user_id"])) // if user is not login
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">login</a> </li>
+                              <li class="nav-item"><a href="registration.php" class="nav-link active">signup</a> </li>
+                              <li class="nav-item"><a href="/online-Plant/admin/index.php" class="nav-link active">Admin Login</a> </li>';
+							}
+						else
+							{
+									//if user is login
+									
+									echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">your orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">logout</a> </li>';
+							}
+
+						?>
+                        </ul>
+						 
+                    </div>
+                </div>
+            </nav>
+            <!-- /.navbar -->
+        </header>
+        <!-- banner part starts -->
+        <section class="hero bg-image" data-image-src="images/img/Wallpaper.jpg">
+            <div class="hero-inner">
+                <div class="container text-center hero-text font-white">
+                    <h1><u>LEAF NOW </u></h1>
+                    <h5 class="font-white space-xs">Ever since industrialization started the care for plants has made it to our least priority
+                                                    list, Even though mankind requires plants to balance the Plant chain that he’s a part of,
+                                                    but the plant enthusiasts are everywhere, be it big metropolitan cities or rural areas, so
+                                                    we aim to bring in a platform that can connect these plant/nature enthusiasts and help
+                                                    them exchange/sell them plants or donate seeds/plants as part of the green initiative
+                                                    that we started 2 years back.</h5>
+                </div>
+            </div>
+            <!--end:Hero inner -->
+        </section>
+        <!-- banner part ends -->
+      
+	  
+	
+        <!-- Popular block starts -->
+        <section class="popular">
+            <div class="container">
+                <div class="title text-xs-center m-b-30">
+                    <h2>Popular Plants of the Month</h2>
+                    <p class="lead">The easiest way to your favourite Plant</p>
+                </div>
+                <div class="row">
+
+						<?php 
+						// fetch records from database to display popular first 3 Plants from table
+						$query_res= mysqli_query($db,"select * from Plants LIMIT 3"); 
+									      while($r=mysqli_fetch_array($query_res))
+										  {
+													
+						                       echo '  <div class="col-xs-12 col-sm-6 col-md-4 Plant-item">
+														<div class="Plant-item-wrap">
+															<div class="figure-wrap bg-image" data-image-src="admin/plants_img/plants/'.$r['img'].'">
+																<div class="distance"><i class="fa fa-pin"></i>1240m</div>
+																<div class="rating pull-left"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-o"></i> </div>
+																<div class="review pull-right"><a href="#">198 reviews</a> </div>
+															</div>
+															<div class="content">
+																<h5><a href="Plants.php?res_id='.$r['rs_id'].'">'.$r['title'].'</a></h5>
+																<div class="product-name">'.$r['slogan'].'</div>
+																<div class="price-btn-block"> <span class="price">₹'.$r['price'].'</span> <a href="Plants.php?res_id='.$r['rs_id'].'" class="btn theme-btn-dash pull-right">Order Now</a> </div>
+															</div>
+															
+														</div>
+												</div>';
+													
+										  }
+						
+						
+						?>
+				
+                 
+                </div>
+            </div>
+        </section>
+        <section class="featured-store">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="title-block pull-left">
+                            <h4>Featured Stores</h4> </div>
+                    </div>
+                    <div class="col-sm-8">
+                        
+                        <div class="store-filter pull-right">
+                            <nav class="primary pull-left">
+                                <ul>
+                                    <li><a href="#" class="selected" data-filter="*">all</a> </li>
+									<?php 
+									// display categories here
+									$res= mysqli_query($db,"select * from plant_category");
+									      while($row=mysqli_fetch_array($res))
+										  {
+											echo '<li><a href="#" data-filter=".'.$row['c_name'].'"> '.$row['c_name'].'</a> </li>';
+										  }
+									?>
+                                   
+                                </ul>
+                            </nav>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="store-listing">
+                        
+						
+						<?php  //fetching records from table and filter using html data-filter tag
+						$ress= mysqli_query($db,"select * from store");  
+									      while($rows=mysqli_fetch_array($ress))
+										  {
+													$query= mysqli_query($db,"select * from plant_category where c_id='".$rows['c_id']."' ");
+													 $rowss=mysqli_fetch_array($query);
+						
+													 echo ' <div class="col-xs-12 col-sm-12 col-md-6 single-store all '.$rowss['c_name'].'">
+														<div class="store-wrap">
+															<div class="row">
+																<div class="col-xs-12 col-sm-3 col-md-12 col-lg-3 text-xs-center">
+																	<a class="store-logo" href="plants.php?res_id='.$rows['rs_id'].'" > <img src="admin/Plants_img/'.$rows['image'].'" alt="store logo"> </a>
+																</div>
+																<!--end:col -->
+																<div class="col-xs-12 col-sm-9 col-md-12 col-lg-9">
+																	<h5><a href="plants.php?res_id='.$rows['rs_id'].'" >'.$rows['title'].'</a></h5> <span>'.$rows['address'].'</span>
+																	<div class="bottom-part">
+																		<div class="cost"><i class="fa fa-check"></i> Min ₹ 10,00</div>
+																		<div class="mins"><i class="fa fa-motorcycle"></i> 30 min</div>
+																		<div class="ratings"> <span>
+																				<i class="fa fa-star"></i>
+																				<i class="fa fa-star"></i>
+																				<i class="fa fa-star"></i>
+																				<i class="fa fa-star"></i>
+																				<i class="fa fa-star-o"></i>
+																			</span> (122) </div>
+																	</div>
+																</div>
+																<!-- end:col -->
+															</div>
+															<!-- end:row -->
+														</div>
+														
+													</div>';
+										  }
+						
+						
+						?>
+						
+							
+						
+					
+                    </div>
+                </div>
+                
+               
+            </div>
+        </section>
+        
+        
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/Plantpicky.min.js"></script>
+</body>
+
+</html>
